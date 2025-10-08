e0fea446007315c56d9de678ffb1992cf8c30374
Divyansh-arma <sharmadivyansh003@gmail.com>
2025-10-08 21:04:19 +0530

    k8s creation terraform code

---
+++ main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+  }
+}
+
+# Configure the DigitalOcean Provider
+provider "digitalocean" {
+  token = var.do_token
+}
+
+# Create a Kubernetes cluster
+
+resource "digitalocean_kubernetes_cluster" "py-microservices-cluster" {
+  name   = "py-microservices-cluster"
+  region = "BLR1"
+
+  version = "latest"
+
+  node_pool {
+    name       = "python-app-pool"
+    size       = "s-1vcpu-2gb"
+    auto_scale = true
+    min_nodes  = 1
+    max_nodes  = 2
+  }
+}
