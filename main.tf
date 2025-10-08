0627847e045a86b93a2f7db36e48080031ae6b61
Divyansh-arma <sharmadivyansh003@gmail.com>
2025-10-08 21:19:10 +0530

    updated the region name and also added th etfstate.lock file to git ignore

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ provider "digitalocean" {
 
 resource "digitalocean_kubernetes_cluster" "py-microservices-cluster" {
   name   = "py-microservices-cluster"
-  region = "BLR1"
+  region = "blr1"
 
   version = "latest"
 
