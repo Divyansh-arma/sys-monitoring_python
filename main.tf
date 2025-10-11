112908daf31d9412b638bf758790911cf74c9406
Divyansh-arma <sharmadivyansh003@gmail.com>
2025-10-11 13:44:55 +0530

    added the registry creation before k8s creation

--- main.tf
+++ main.tf
@@ -12,6 +12,17 @@ provider "digitalocean" {
   token = var.do_token
 }
 
+# Create a new container registry
+resource "digitalocean_container_registry" "smoke" {
+  name                   = "smoke"
+  subscription_tier_slug = "starter"
+  region                 = var.region
+}
+
+resource "digitalocean_container_registry_docker_credentials" "example" {
+  registry_name = digitalocean_container_registry.smoke.name
+}
+
 # Create a Kubernetes cluster
 
 resource "digitalocean_kubernetes_cluster" "py-microservices-cluster" {
